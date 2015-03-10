2171549a1b50e9c6a51be94efbc20b904bee6354
Jmads13 <puklain2004@hotmail.com>
2015-03-10 14:43:53 +0100

    TRD core

---
+++ TRDMeta/Core/src/main/java/dk/sdu/mmmi/cbse/core/TRDMain.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dk.sdu.mmmi.cbse.core;
+
+import playn.core.Game;
+
+/**
+ *
+ * @author SoA
+ */
+public class TRDMain extends Game.Default{
+
+    public TRDMain(int updateRate) {
+        super(updateRate);
+    }
+
+    @Override
+    public void init() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public void update(int delta) {
+        
+    }
+    
+    @Override
+    public void paint(float alpha){ 
+        
+    }
+    
+    
+}
